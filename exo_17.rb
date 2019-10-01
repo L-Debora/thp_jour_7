b10ca1078b697387e0936377ba0e2ce6742f8714
Debora <debora.v@hotmail.fr>
2019-10-01 22:19:03 +0200

    les commentaires test en moins c'est toujours mieux

--- exo_17.rb
+++ exo_17.rb
@@ -1,20 +1,3 @@
-=begin
-puts "Quel est ton age ?"
-print ">"
-user_age = gets.chomp.to_i
-user_date = 0
-user_agedec = user_age
-
-while user_date <= user_age
-    puts "il y a #{user_agedec} an(s) tu avais #{user_date} an(s)"
-    if user_agedec == user_date 
-        puts "Il y a #{user_agedec} ans, tu avais la moitié de l'âge que tu as aujourd'hui"
-    end
-    user_date += 1
-    user_agedec -= 1
-end 
-=end
-
 puts "Quel est ton age?"
 print ">"
 
